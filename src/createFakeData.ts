5f297cfbf8693590f89966a7b3176fb9129abe88
Chickenbumps <whgotjd46@gmail.com>
2022-05-31 22:59:07 +0900

    Update createFakeData.ts

--- src/createFakeData.ts
+++ src/createFakeData.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { currentTime } from "./utils/utils";
 
 const prisma = new PrismaClient();
 
@@ -8,8 +7,8 @@ export default async function createFakeData() {
     userId: 1,
     title: `포스트 #${i}`,
     body: "cream twilight eunoia banana apple eunoia heimish shine flutter cherish adorable blossom girlish miracle grapes lucid miracle adorable serendipity ice ideale ice droplet world pure flutter cresent requiem milky marshmallow honey iris laptop heimish world twilight laptop iris shine hello honey laptop cresent destiny pure world banana lovable haze carnival.twilight bijou hello miracle droplet honey milky eunoia grapes destiny flutter serendipity kitten bijou blush ice twinkle bijou charming droplet hello florence eunoia ice flora milky blush vanilla charming world kitten flora eunoia twinkle honey miracle banana haze girlish you like kitten grapes honey adorable blush seraphic honey ideale banana.haze world cream ice ice honey pure iris moonlight sunrise adolescence vanilla blossom stella droplet twilight blossom aurora aurora seraphic banana vanilla way lovable adolescence fascinating blush lucid way cherish flora cherish serendipity shine cresent like cream adolescence blossom milky lucy blossom fascinating way blossom apple milky twilight charming cresent.",
-    createdAt: currentTime(),
-    updatedAt: currentTime(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }));
 
   const fakeDatas = await prisma.post.createMany({
