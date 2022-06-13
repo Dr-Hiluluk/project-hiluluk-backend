c06a1884cc6f573f9dc4fea66fb0c0dbaf5f2e52
Chickenbumps <whgotjd46@gmail.com>
2022-06-13 22:30:48 +0900

    Feat: 추가 파일 API
    - create-url: 접근가능한 file url 생성
    - upload: s3에 file upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import userRouter from "./auth/auth.route";
 import commentRouter from "./comment/comment.route";
+import fileRouter from "./file/file.route";
 import postRouter from "./post/post.route";
 
 const router: Router = express.Router();
@@ -8,4 +9,6 @@ const router: Router = express.Router();
 router.use("/auth", userRouter);
 router.use("/post", postRouter);
 router.use("/comment", commentRouter);
+router.use("/file", fileRouter);
+
 export default router;
