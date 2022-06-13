dac6766545587752ef7bbf68adfb0d8256357407
Chickenbumps <whgotjd46@gmail.com>
2022-06-13 22:25:02 +0900

    !HOTFIX: 수정 글내용에 이미지 추가 가능하게
    - 글내용에 이미지 추가할 수 있도록 기존 body sanitizeHtml 삭제

--- src/router/post/post.ctrl.ts
+++ src/router/post/post.ctrl.ts
@@ -111,15 +111,12 @@ class PostController {
     try {
       const postId = parseInt(req.params.postId, 10);
       const { title, body, tags } = req.body;
+      console.log("req.body.body:", req.body.body);
 
-      let filteredBody = body;
-      if (body) {
-        filteredBody = sanitizeHtml(body, sanitizeOption);
-      }
       const updatePostStatus = await PostService.updatePost({
         postId,
         title,
-        body: filteredBody,
+        body,
         tags,
       });
 
